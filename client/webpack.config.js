efbd7c2dd019f8e95c44411d2d49828f65c221dc
Nathan Cuevas <nathanacuevas97@gmail.com>
2023-08-15 18:41:11 -0700

    Created the webpack rules to handle css files images and new Javascript syntax with babel

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
@@ -18,12 +18,26 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-
+      new MiniCSSExtractPlugin(),
     ],
 
     module: {
       rules: [
-
+        {
+          test: /\.css$/i,
+          use: [MiniCSSExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.m?js$/i,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          }
+        }
       ],
     },
   };
