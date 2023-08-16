c70fd929d1a6b7b7d094f36957eb3c192f5763ca
Nathan Cuevas <nathanacuevas97@gmail.com>
2023-08-16 15:17:51 -0700

    Fixed error when their was no initial content to grab out of the database results

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -34,6 +34,6 @@ export const getDb = async () => {
   const request = store.get(1);
   const result = await request;
   console.log('result.value', result);
-  return result.content;
+  return result?.content;
 };
 initdb();
